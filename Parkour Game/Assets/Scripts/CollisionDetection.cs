8904609b41c6b2bc11df4f1e4a0e070cb7f07263
Lucio Bollettieri <lcbollettie22@blakeschool.org>
2018-09-20 14:00:41 -0500

    First Final Version

---
+++ Parkour Game/Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour {
+
+
+	void OnTriggerEnter (Collider other) {
+
+        Debug.Log("Object is within trigger");
+        transform.Translate(new Vector3(0, 0, 0));
+
+	}
+}
---
+++ Parkour Game/Assets/Scripts/WASDMovementCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDMovementCube : MonoBehaviour {
+
+    public float xSpeed;
+
+    void Start () {
+
+        xSpeed = 7f;
+
+	}
+	
+	void Update () {
+
+        transform.Translate(xSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+
+	}
+}
